a2477858323f2f360c911dcd234c8c85ee0f4b1e
Etienne Leriche <etienneleriche@gmail.com>
2023-02-15 01:22:32 +0100

    main: add contactform

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,78 +1,122 @@
 // /* Main scripts */
 window.addEventListener("DOMContentLoaded", () => {
 
-/* Mobile menu */
-let path = document.querySelector(".path");
+    /* Mobile menu */
+    let path = document.querySelector(".path");
 
-function lerp(start, end, t){
-    return start * (1 - t) + end * t;
-}
-
-let toggle = false;
-
-// Start SVG at bottom of screen
-let y = 100;
-let c = 100;
-
-
-function animate(){
-    if(toggle){
-        y = lerp(y, 0, .055);
-        c = lerp(c, 0, 0.075);
-        path.setAttribute('d', `M 0 ${y} L 0 100 100 100 100 ${y} C ${50} ${c}, ${50} ${c}, 0 ${y}` )
-    }else{
-        y = lerp(y, 100, .055)
-        c = lerp(c, 100, 0.075);
-        path.setAttribute('d', `M 0 ${y} L 0 100 100 100 100 ${y} C 50 ${c}, ${50} ${c}, 0 ${y}` )
+    function lerp(start, end, t) {
+        return start * (1 - t) + end * t;
     }
-    
-    requestAnimationFrame(animate)
-}
 
-animate()
+    let toggle = false;
 
-const toggleButton = document.querySelector('.menu-tog-wrapper');
-const header = document.querySelector('#main-header');
-let menuToggle = document.querySelector('.menu-tog');
-let menuWrapper = document.querySelector('.wrapper');
-let nav = document.querySelector('#mobile-nav');
-toggleButton.addEventListener('click', () => {
-    setTimeout(() => {
-        toggle = !toggle;
-       
-    }, 300)
-    if(toggle){
-        nav.classList.remove('active');
+    // Start SVG at bottom of screen
+    let y = 100;
+    let c = 100;
 
-    }else{
+
+    function animate() {
+        if (toggle) {
+            y = lerp(y, 0, .055);
+            c = lerp(c, 0, 0.075);
+            path.setAttribute('d', `M 0 ${y} L 0 100 100 100 100 ${y} C ${50} ${c}, ${50} ${c}, 0 ${y}`)
+        } else {
+            y = lerp(y, 100, .055)
+            c = lerp(c, 100, 0.075);
+            path.setAttribute('d', `M 0 ${y} L 0 100 100 100 100 ${y} C 50 ${c}, ${50} ${c}, 0 ${y}`)
+        }
+
+        requestAnimationFrame(animate)
+    }
+
+    animate()
+
+    const toggleButton = document.querySelector('.menu-tog-wrapper');
+    let menuToggle = document.querySelector('.menu-tog');
+    let menuWrapper = document.querySelector('.wrapper');
+    let nav = document.querySelector('#mobile-nav');
+    const navLinks = document.querySelectorAll('#mobile-nav a')
+    toggleButton.addEventListener('click', () => {
         setTimeout(() => {
-            nav.classList.add('active')
-        }, 1000)
-    }
+            toggle = !toggle;
+
+        }, 300)
+        if (toggle) {
+            nav.classList.remove('active');
+
+        } else {
+            setTimeout(() => {
+                nav.classList.add('active')
+
+            }, 1000)
+        }
+
+        menuToggle.classList.toggle('active')
+        menuWrapper.classList.toggle('active')
+
+    })
+    for (const link of navLinks) {
+        link.addEventListener('click', () => {
+
+            setTimeout(() => {
+                toggle = !toggle;
+
+            }, 300)
+
+            if (!toggle) {
+                nav.classList.add('active');
     
-    menuToggle.classList.toggle('active')
-    menuWrapper.classList.toggle('active')
-    header.classList.toggle('active')
-})
+            } else {
+                setTimeout(() => {
+                    nav.classList.remove('active')
+    
+                }, 250)
+            }
 
-/* Mouse scroll */
-const scrollDownMouse = document.getElementById('scroll-down-mouse')
-const scrollTopBtn = document.getElementById('scroll-top-btn')
-
-window.addEventListener('scroll', function () {
-    scrollFunction();
-});
-
-//Scroll
-function scrollFunction() {
-    if (window.scrollY >= 70) {
-        scrollTopBtn.classList.add('active');
-        scrollDownMouse.classList.add('scrolled');
+            menuToggle.classList.toggle('active')
+            menuWrapper.classList.toggle('active')
+        })
     }
-    if (window.scrollY === 0) {
-        scrollTopBtn.classList.remove('active');
-        scrollDownMouse.classList.remove('scrolled');
+
+
+    /* Mouse scroll */
+    const scrollDownMouse = document.getElementById('scroll-down-mouse')
+    const scrollTopBtn = document.getElementById('scroll-top-btn')
+
+    window.addEventListener('scroll', function () {
+        scrollFunction();
+    });
+
+    //Scroll
+    function scrollFunction() {
+        if (window.scrollY >= 70) {
+            scrollTopBtn.classList.add('active');
+            scrollDownMouse.classList.add('scrolled');
+        }
+        if (window.scrollY === 0) {
+            scrollTopBtn.classList.remove('active');
+            scrollDownMouse.classList.remove('scrolled');
+        }
     }
-}
+
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (() => {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+    })()
 
 })
